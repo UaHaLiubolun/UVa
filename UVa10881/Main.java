bfb4b1e6c01dc958a6bcb70c45054014b6bf4256
System Administrator <root@macbook.local>
2017-02-05 21:53:17 +0800

    UVa10881

--- UVa10881/Main.java
+++ UVa10881/Main.java
@@ -1,8 +1,23 @@
 package UVa.UVa10881;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static int Num;
+    private static int[] L;
+    private static int[] T;
+    private static int[] N;
+    private static int[][] Ants;
 
     public static void main(String args[]) {
 
+        Scanner input = new Scanner(System.in);
+        Num = input.nextInt();
+
+        for (int i = 0; i < Num; i++) {
+
+        }
     }
 }
+
