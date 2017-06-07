6cae9bfc5d7d9bb75f26ceff7aaee6706c13485c
GiveMeStar <liubolunliuyao@gmail.com>
2017-06-07 10:26:47 +0800

    战舰数量

---
+++ leetCode/BattleshipsInABoard.java
@@ -0,0 +1,28 @@
+package UVa.leetCode;
+
+/**
+ * Created by jhonlambo on 2017/6/7.
+ */
+public class BattleshipsInABoard {
+    /**
+     * 由于给的测试用例有要求限定 所以只要 左 或者 上 不是X就可以直接加1
+     * @param board
+     * @return
+     */
+    public int countBattleships(char[][] board) {
+        int m = board.length;
+        if (m == 0) return 0;
+        int n = board[0].length;
+
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == '.') continue;
+                if (i > 0 && board[i - 1][j] == 'X') continue;
+                if (j > 0 && board[i][j - 1] == 'X') continue;
+                count++;
+            }
+        }
+        return count;
+    }
+}
